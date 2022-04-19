7b883dd7f14b01465090f385b09d0bb3a25b473e
Millie Swain <millie.swain@gmail.com>
2022-04-19 16:30:45 -0700

    main.js

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const links = [ 
+    {
+    label: "Week1 notes",
+    url: "week1/index.html"
+
+}
+]
+
+function loadIndex() {
+    const ol = document.querySelector("#linksList");
+
+    links.forEach( link => {
+        const li = document.createElement("li");
+        const href = document.createElement("a");
+        href.setAttribute("href", link.url);
+        href.innerText = link.label;
+
+        li.appendChild(href);
+        ol.appendChild(li)
+    })
+}
