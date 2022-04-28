d275182dc772c9ecdaada83c10fd98bb3a66781f
Millie Swain <millie.swain@gmail.com>
2022-04-27 21:34:26 -0700

    main js

--- week02/quiz/main.js
+++ week02/quiz/main.js
@@ -1,1 +1,26 @@
-alert('Welcome to Quiz Ninja!');
+const question = "What is Superman's real name?"
+const answer = prompt(question);
+alert(`You answered ${answer}`);
+
+// alert('Welcome to Quiz Ninja!');
+
+
+const quiz = [
+    ["What is Superman's real name?","Clark Kent"],
+    ["What is Wonder Woman's real name?","Diana Prince"],
+    ["What is Batman's real name?","Bruce Wayne"]
+];
+let score = 0 // initialize score
+
+for(const [question,answer] of quiz){
+    const response = prompt(question);
+    if(response === answer){
+        alert('Correct!');
+        score++;
+    } else {
+        alert(`Wrong! The correct answer was ${answer}`);
+    }
+}
+// At the end of the game, report the player's score
+alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
+
