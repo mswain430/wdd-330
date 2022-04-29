10208c5fde18d8984d5ca5636227e67c7c24fe2a
Millie Swain <millie.swain@gmail.com>
2022-04-28 23:11:14 -0700

    quiz

--- week02/quiz/main.js
+++ week02/quiz/main.js
@@ -24,3 +24,36 @@ for(const [question,answer] of quiz){
 // At the end of the game, report the player's score
 alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
 
+
+
+function start(quiz){
+    let score = 0;
+
+    // main game loop
+    for(const [question,answer] of quiz){
+        const response = ask(question);
+        check(response,answer);
+    }
+    // end of main game loop
+
+    gameOver();
+
+    // function declarations
+    function ask(question){
+        return prompt(question);
+    }
+
+    function check(response,answer){
+        if(response === answer){
+        alert('Correct!');
+        score++;
+        } else {
+        alert(`Wrong! The correct answer was ${answer}`);
+        }
+    }
+
+    function gameOver(){
+        alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
+    }
+}
+start(quiz);
