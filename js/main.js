4df842885ee0fa48ad495be7a33072c6e6d74d51
Millie Swain <millie.swain@gmail.com>
2022-05-26 17:51:26 -0700

    more

--- js/main.js
+++ js/main.js
@@ -5,47 +5,47 @@ const links = [
 },
 {
     label: "Week2 notes",
-    url: "week02/index.html", 
+    url: "week02/index.html",
 },
 {
     label: "Week3 notes",
-    url: "week03/index.html", 
+    url: "week03/index.html",
 },
 {
     label: "Week4 notes",
-    url: "week04/index.html", 
+    url: "week04/index.html",
 },
 {
-    label: "Wee5 notes",
-    url: "week05/index.html", 
+    label: "Week5 notes",
+    url: "week05/index.html",
 },
 {
     label: "Week6 notes",
-    url: "week06/index.html", 
+    url: "week06/index.html",
 },
 {
     label: "Week7 notes",
-    url: "week07/index.html", 
+    url: "week07/index.html",
 },
 {
     label: "Week8 notes",
-    url: "week08/index.html", 
+    url: "week08/index.html",
 },
 {
     label: "Week09 notes",
-    url: "week9/index.html", 
+    url: "week9/index.html",
 },
 {
     label: "Week10 notes",
-    url: "week10/index.html", 
+    url: "week10/index.html",
 },
 {
     label: "Week11 notes",
-    url: "week11/index.html", 
+    url: "week11/index.html",
 },
 {
     label: "Week12 notes",
-    url: "week12/index.html", 
+    url: "week12/index.html",
 },
 
 ]
