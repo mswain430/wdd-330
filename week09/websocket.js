21b1b59643cb2c4a2bcbb2bb68da8a8610feb508
Millie Swain <millie.swain@gmail.com>
2022-06-20 23:32:27 -0700

    notes

---
+++ week09/websocket.js
@@ -0,0 +1,55 @@
+const URL = 'wss://echo.websocket.org/';
+const outputDiv = document.getElementById('output');
+const form = document.forms[0];
+const connection = new WebSocket(URL);
+
+connection.addEventListener('open', () => {
+    output('CONNECTED');
+}, false);
+
+function output(message) {
+    const para = document.createElement('p');
+    para.innerHTML = message;
+    outputDiv.appendChild(para);
+}
+
+form.addEventListener('submit', message, false);
+
+function message(event) {
+    event.preventDefault();
+    const text = form.message.value;
+    output(`SENT: ${text}`);
+}
+connection.addEventListener('message', (event) => {
+    output(`RESPONSE: ${event.data}`);
+}, false);
+
+connection.addEventListener('close', () => {
+    output('DISCONNECTED');
+}, false);
+
+connection.addEventListener('error', (event) => {
+output(`<span style='color: red;'>ERROR: ${event.data}</span>`);
+}, false);
+
+if(window.Notification) {
+    Notification.requestPermission();
+}
+if(window.Notification) {
+    Notification.requestPermission()
+    .then((permission) => {
+        if(Notification.permission === 'granted') {
+        new Notification('Hello JavaScript!');
+        }
+    });
+}
+const notification = new Notification('JavaScript: Novice to Ninja',{
+    body: 'The new book from SitePoint',
+    icon: 'sitepointlogo.png'
+});
+
+notification.close();
+
+notification.addEventListener('click', () => {
+window.open('https://sitepoint.com')
+}, false);
