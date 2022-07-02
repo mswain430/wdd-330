3112f1531d9e24a6a4a2968ee6d488e2788336c9
Millie Swain <millie.swain@gmail.com>
2022-07-02 11:03:47 -0700

    auth

--- week11/client/js/auth.js
+++ week11/client/js/auth.js
@@ -13,70 +13,68 @@ export default class Auth {
         // replace the ids below with whatever you would like
         const username = document.getElementById('username');
         const password = document.getElementById('password');
-
         const postData = {
             email: username.value,
             password: password.value
         };
         try {
-            // 1. use the makeRequest function to pass our credentials to the server
-            const data = await makeRequest('login', 'POST', postData);
-            // 2. if we get a successful response...we have a token!  Store it since we will need
-             this.jwtToken = data.accessToken;
+          // 1. use the makeRequest function to pass our credentials to the server
+          const data = await makeRequest('login', 'POST', postData);
+          // 2. if we get a successful response...we have a token!  Store it since we will need
+          this.jwtToken = data.accessToken;
             // let's get the user details as well and store them locally in the class
            // you can pass a query to the API by appending it on the end of the url like this: 'users?email=' + email
-            this.user = await this.getCurrentUser(username.value);
+          this.user = await this.getCurrentUser(username.value);
             // hide the login form
-           console.log(data);
+          console.log(data);
 
            // document.getElementById('login').classList.add('hidden');
-           hideLogin();
+          hideLogin();
            //clear the password
-           password.value = '';
+          password.value = '';
 
-           this.errors.clearError();
+          this.errors.clearError();
 
          //since we have a token lets go get
           callback();
-
-      } catch (error){
-        //if there were any errors display
-        this.errors.handleError(error);
-        console.log(error);
+        } catch (error){
+         //if there were any errors display
+          this.errors.handleError(error);
+          console.log(error);
       }
     }
     // uses the email of the currently logged in person
     async getCurrentUser(email){
         try {
             // 3. add the code here to make a request for the user identified by email...don't forget to send the token!
-            const data = await makeRequest(
-                'users?email=' + email,
-                'GET',
-                null,
-                this.jwtToken
-            );
-            console.log(data);
-            return data[0];
+          const data = await makeRequest(
+            'users?email=' + email,
+            'GET',
+            null,
+            this.jwtToken
+           );
+          console.log(data);
+          return data[0];
         } catch (error) {
             // if there were errors display
-            this.errors.handleError(error);
+          this.errors.handleError(error);
 
-            console.log(error);
+          console.log(error);
         }
     }
     async updateUser() {
-        // after logging in we pull down the user from api
-        this.user.age = 40;
-        try {
-            const result = await makeRequest(
-                'users/' + this.user.id,
-                'PUT',
-                this.user,
-                this.jwtToken
-            );
-            console.log('Update user:', result);
-        } catch(error){
-            this.errors.handleError(error,showLogin);
+      // after logging in we pull down the user from api
+      this.user.age = 40;
+      try {
+        const result = await makeRequest(
+          'users/' + this.user.id,
+          'PUT',
+          this.user,
+          this.jwtToken
+          );
+         console.log('Update user:', result);
+       } catch(error){
+         this.errors.handleError(error,showLogin);
         }
     }
     set token(value) {
