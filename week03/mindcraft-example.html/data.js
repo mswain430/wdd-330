2dd0768daf907d797d22c1fa2aace5d7c8744eaa
Millie Swain <millie.swain@gmail.com>
2022-07-02 22:12:32 -0700

    week 3 tweaks

--- week03/mindcraft-example.html/data.js
+++ week03/mindcraft-example.html/data.js
@@ -1,26 +1,27 @@
 let minecraftData = [
     {
         type: "Creeper",
-        img: "256px-Creeper.png",
+        img: "img/256px-Creeper.png",
         desc: "Creepers are common green, deadly hostile mobs that silently approach players and explode shortly after coming within 3 blocks of their targets. Due to their distinctive appearance and high potential for killing unwary players as well as damaging the environment and players' constructions, creepers have become one of the icons of Minecraft, notorious both among players and non-players.\nCreepers are a major source of gunpowder as well as the only way to obtain most music discs. When struck by lightning, a creeper becomes charged, which substantially amplifies its explosion power and enables mob heads to be obtained.",
         spawning: "Creepers naturally spawn in the Overworld on solid blocks with a light level of 7 or less."
     },
     {
         type: "Zombie",
-        img:"326px-Zombie.png",
+        img:"img/326px-Zombie.png",
         desc: "Zombies are common undead hostile mobs that attack players, villagers, and turtle eggs. They burn in the daylight, similar to other undead mobs.",
         spawning: "In the Overworld, zombies spawn in groups of 4 at a light level of 7 or less. In deserts, all zombies exposed to the sky have an 80%‌[JE only] / 70%‌[BE only] chance to be replaced by husks, a zombie variant. Zombies that are not husks have a 5% chance to spawn as a zombie villager while all zombie variants also have a 5% chance to spawn as a baby zombie type. Baby zombies have an additional 5% chance of spawning as a chicken jockey.‌[JE only] Baby zombies have a 15% chance to want to be a jockey, and if they do, then upon nearing the player and before attacking, they check for one of the following and mount it: adult zombies, chickens, sheep, cows, pigs, llamas, ocelots, foxes, pandas, mooshrooms, untamed cats, untamed wolves, horses and variants, spiders and cave spiders.‌[BE only] A zombie can also spawn from a husk that drowns in water but converts to a drowned if its head remains submerged."
     },
     {
         type: "Iron Golem",
-        img: "300px-Iron_Golem.png",
+        img: "img/300px-Iron_Golem.png",
         desc: "Iron golems are large, strong utility mobs that defend players and villagers.",
         spawning: 'In Java Edition, villagers can summon iron golems, either when they are gossiping or panicking. When a villager tries to summon a golem, up to 10 attempts are made to spawn a golem within a 16x13x16 box centered on the villager (villager block position +7/-8 blocks along x/z axes and +6/-6 blocks along y axis). A random column is picked and then the topmost block in that column is selected that is air, water, or lava, and has a "solid-blocking" block underneath, or block at y=+6 if none such is available in that column. The target location is then checked whether the block underneath has a solid top surface (which is not the same as "solid-blocking"). The target block and 2 blocks above must not be a full block nor be redstone-powered nor be rails, and the two blocks above must not be water. This means the iron golem can spawn inside 1-deep water or inside blocks like halfslabs, fences, and carpets (if other checks pass). Adjacent blocks are irrelevant, so golems can even spawn partially inside adjacent solid blocks resulting in their own suffocation and death.[1] However, the spawning iron golem still must not collide with any existing mobs. In Bedrock Edition, an iron golem can spawn naturally when a village first generates in the world. Iron golems also spawn in villages having at least 21 beds and 10 villagers, in a 16×6×16 volume around the village center defined by a bed, a bell, or another meeting point. The chance of spawning is 1⁄7000 per game tick, which averages to one every 5 minutes and 50 seconds. Iron golems can spawn provided the blocks it spawns in are non-solid and the block it spawns on has a solid flat top surface.'
     },
     {
         type: "Skeleton",
-        img: "300px-Skeleton_Aiming.png",
+        img: "img/300px-Skeleton_Aiming.png",
         desc: "Skeletons are common undead hostile mobs equipped with bows.",
         spawning: "Skeletons can spawn in the overworld at a light level of 7 or less, in groups of 1-4. 80% of skeletons spawned directly under the sky spawn as strays in Snowy Tundra, Snowy Mountains, Ice Spikes, Frozen River, Frozen Ocean, and Deep Frozen Ocean biomes. Spiders have a 1% chance to spawn with a skeleton riding them, creating a spider jockey. Skeleton horsemen spawn when a skeleton trap is triggered. The skeletons ride on skeleton horses and are equipped with enchanted bows and iron helmets, and have damage immunity for 3 seconds after spawning."
     }
+
 ]
