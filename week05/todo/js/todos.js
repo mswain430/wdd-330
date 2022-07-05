b48154dc6817bda0a63db663d3888ec4d19fa033
Millie Swain <millie.swain@gmail.com>
2022-07-04 18:33:25 -0700

    commit A O F

--- week05/todo/js/todos.js
+++ week05/todo/js/todos.js
@@ -24,10 +24,20 @@ fetch("quotes.json")
         const randomNum = Math.floor(Math.random() * quotes.length);
         const inspiration = quotes[randomNum];
 
-        document.querySelector('#verse').innerText = '"' + inspiration.verse + '"';
-        document.querySelector('#author').innerText = '~' + inspiration.reference;
+        document.querySelector('#verse').innerText = '"' + inspiration.quote + '"';
+        document.querySelector('#author').innerText = '~' + inspiration.author;
 })
 
+fetch("articlesOfFaith.json")
+  .then(data => data.json())
+  .then(articlesOfFaith => {
+        const randomNum = Math.floor(Math.random() * articlesOfFaith.length);
+        const inspiration = articlesOfFaith[randomNum];
+
+        document.querySelector('#reference').innerText = '"' + inspiration.article + '"';
+        document.querySelector('#article').innerText = '~' + inspiration.verse;
+        document.querySelector('#verse').innerText = '~' + inspiration.reference;
+})
 loadTodos();
 
 function addNewTodo(e) {
