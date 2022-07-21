2281b10026c4a2988c041757f60364c29dbcd6e8
Millie Swain <millie.swain@gmail.com>
2022-07-21 01:27:34 -0700

    transitions

--- week12/flowers/script.js
+++ week12/flowers/script.js
@@ -4,8 +4,10 @@ fetch("flowerquotes.json")
   .then(flowerquotes => {
     const randomNum = Math.floor(Math.random() * flowerquotes.length);
     const inspiration = flowerquotes[randomNum];
+
     document.querySelector('#quote').innerText = '"' + inspiration.quote + '"';
     document.querySelector('#author').innerText = ` ~ ${inspiration.author}`;
+
 });
 }
 loadquotes();
