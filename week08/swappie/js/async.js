638829abbf2f00747a8a784f2c85e0cf086d5678
Millie Swain <millie.swain@gmail.com>
2022-07-22 16:43:41 -0700

    more

--- week08/swappie/js/async.js
+++ week08/swappie/js/async.js
@@ -113,7 +113,7 @@ async function getShipDetails(url) {
 }
 showShips();
 
-function toggleMenu() {
+/* function toggleMenu() {
  document.getElementById("button").classList.toggle("open");
   console.log("It worked!")
 }
@@ -125,4 +125,30 @@ window.onload = () => {
         this.parentNode.remove()
         return false;
     };
-};
+}; */
+
+// Get the modal
+/* let modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+let btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} */
