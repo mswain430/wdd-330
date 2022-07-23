83e7da7caf0a0be25aa618b6a20080c8d6babd1c
Millie Swain <millie.swain@gmail.com>
2022-07-22 22:26:17 -0700

    format

--- week1/customer.js
+++ week1/customer.js
@@ -1,24 +1,24 @@
 function addAndSaveCustomer(){
-    const aName = document.getElementById("name_input").value;
-    const anAge = document.getElementById("age_input").value * 1;
-    const anInseam = document.getElementById("inseam_input").value * 1;
-    let aClothingCustomer = {
+  const aName = document.getElementById("name_input").value;
+  const anAge = document.getElementById("age_input").value * 1;
+  const anInseam = document.getElementById("inseam_input").value * 1;
+  let aClothingCustomer = {
         "name":aName,
         "age":anAge,
         "inseam":anInseam
     }
-    let allCustomers = null;
-    let storedCustomersString = localStorage["all_customers"];
-    if(storedCustomersString == null){
+  let allCustomers = null;
+  let storedCustomersString = localStorage["all_customers"];
+  if(storedCustomersString == null){
       allCustomers = []
+  }
+  else{
+     allCustomers = JSON.parse(storedCustomersString);
     }
-    else{
-      allCustomers = JSON.parse(storedCustomersString);
-    }
-    allCustomers.push(aClothingCustomer);
-    let allCusomersString = JSON.stringify(allCustomers);
-    localStorage["all_customers"] = allCusomersString;
-    showAllCustomers();
+  allCustomers.push(aClothingCustomer);
+  let allCusomersString = JSON.stringify(allCustomers);
+  localStorage["all_customers"] = allCusomersString;
+  showAllCustomers();
 }
 
 document.getElementById("name_input").value = null;
@@ -33,10 +33,10 @@ function showAllCustomers(){
     customerDisplayer.innerHTML = null;
     let numberOfCustomers = allCustomers.length;
   for (let i = 0; i < allCustomers.length; i++) {
-        let aClothingCustomer = allCustomers[i]
-        customerDisplayer.innerHTML += `<hr><p>Customer: ${aClothingCustomer["name"]}</p>
-        <p>Age:${aClothingCustomer["age"]}</p>
-        <p>Inseam: ${aClothingCustomer["inseam"]}</p>`;
-        }
+    let aClothingCustomer = allCustomers[i]
+    customerDisplayer.innerHTML += `<hr><p>Customer: ${aClothingCustomer["name"]}</p>
+    <p>Age:${aClothingCustomer["age"]}</p>
+    <p>Inseam: ${aClothingCustomer["inseam"]}</p>`;
+    }
   }
 }
