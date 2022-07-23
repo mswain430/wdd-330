f44e39a2e688f861f4b5c6f66c9144c089667dd5
Millie Swain <millie.swain@gmail.com>
2022-07-22 17:34:22 -0700

    comments

--- week07/hikes5/app7.js
+++ week07/hikes5/app7.js
@@ -1,5 +1,10 @@
 import Hikes from './hikes.js';
+import Comments from './comments.js';
 // import Comments from './comments.js';
+const myComments = new Comments('comment');
+window.addEventListener('load', () => {
+  myComments.showCommentList();
+});
 //on load get the array and insert it into the page
 const myHikes = new Hikes('hikes');
 window.addEventListener('load', () => {
